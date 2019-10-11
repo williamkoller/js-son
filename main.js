611317b68ca5b17a03c4401b6febcd782cdc1471
William Koller <wkoller25@gmail.com>
2019-10-10 23:44:41 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,86 @@
+// function getFunction(name)
+// {
+//     console.log('Name => ', name);
+// }
+
+(function check(name) {
+    console.log('Name => ', name);
+})("Koller");
+ // getFunction("Linux");
+
+//console.log('hello from external scripts');
+
+// var age = prompt('How old are you?');
+
+// var ageParser = parseInt(age);
+
+/**
+ *  use condition if
+ */
+
+    // if(ageParser >= 18){
+    //   console.log('You are authorized');
+    // } else {
+    //   console.log('You are not authorized');
+    // }
+
+/**
+ *  use switch
+ */
+
+    // switch(true) {
+    //     case (ageParser >= 18):
+    //       console.log('You are authorized Switch');
+    //     break;
+    //     case (ageParser < 18):
+    //       console.log('You are not authorized Switch');
+    //     break;
+    //     default:
+    //       console.log('default function');
+    //     break;
+    // }
+/**
+ *  use callback
+ */
+
+    // [0,1,2,3,6,4,5,6].forEach(function($value, $key){
+    //   console.log($value);
+    // });
+
+/**
+ * use while
+ */
+    // var i = 0;
+    // while(i < 10){
+    //   console.log("variable I: " + i);
+    //   i++;
+    // }
+/**
+ *  use for
+ */
+    // for (var a = 0; a < 11; a++){
+    //   console.log("variable A: " + a);
+    // }
+
+/**
+ * use var and call console.log
+ */
+
+    // var number = 12;
+    // var number1 = 30;
+
+    // console.log(typeof number);
+
+    // number = "0" + number1;
+
+    // console.log(number);
+    // console.log(typeof number);
+
+    // var myFunction = function()
+    // {
+    //   var number = 0.1 + 2;
+
+    //   console.log(number);
+    // }
+    // myFunction();
+
